5316ab470409def61bb2261392af6658a70958ed
RiyaUpadhyay2311 <166204072+RiyaUpadhyay2311@users.noreply.github.com>
2025-04-22 16:09:00 +0530

    Classes

---
+++ FUCNTION.js
@@ -0,0 +1,54 @@
+//(para1,para2)=>body of function arrow function
+// let doinArrow = x => console.log(x)
+
+// let spread =["so","much","fun"];
+// let message=["Javascript","is",...spread,"and","very"];
+
+// function addTwoNumbers(x,y){
+//     console.log(x+y);
+// }
+// let arr = [5,9];
+// addTwoNumbers(...arr);
+
+// function addNumbers(x,y,z,a){
+//     console.log(x+y+a+z);
+//      }
+//      let arr = [5,9];
+//      let arr1 = [5,9];
+//      addTwoNumbers(...arr,...arr);//
+
+
+// function somefunction(para1,...para2){//for rest para meter 
+//     console.log(para1,para2);
+// }
+// somefunction("hii","hello","bye");
+
+
+
+
+//returning function value
+
+//prompt
+//null=uninitialised undefined = initialised but not to a value
+
+// let result=addTwoNumber(4,5)
+// console.log(result);
+    
+// let addTwoNum=(x,y)=>console.log()
+
+// function addTwoNumbers(x,y){
+//     console.log(x+y);
+//  }
+
+
+// let resultArr= [];
+// for(let i=0;i<10;i++){
+// let result =addTwoNumbers(i,2*i);
+//  resultsArr.push(result);
+// }
+// console.log(resultArr);
+
+
+
+
+
---
+++ classes.js
@@ -0,0 +1,228 @@
+//this development reduce the complexity
+// class ClassName{
+//     constructor(prop1,prop2){
+//         this.prop1 = prop1;//this dot something==class variable
+//         this.prop2=prop2;
+//     }
+// }
+
+
+// let obj = new ClassName("arg1","arg2");
+
+// console.log(obj.prop1);
+// console.log(obj.prop2);
+
+
+
+//this keyword refers to obj is belong to 
+
+
+// class Dog{
+//     constructor(dogName,weight,color,breed){
+//         this.dogName = dogName;
+//         this.weight= weight;
+//         this.color = color;
+//         this.breed = breed;//they are public thats why we are using directly if private then by some methods
+//     }
+// }
+
+// let dog = new Dog("Javascript", 2.4,"brown","chichua");
+// console.log(dog.dogName);
+// console.log(dog.weight);
+// console.log(dog.color);
+// console.log(dog.breed);
+
+
+
+//classname starts with capital
+
+// class Person{
+//     constructor(firstname,lastname){
+//         this.firstname = firstname;
+//         this.lastname = lastname;
+//     }
+// }
+
+// let p= new Person("Riya","Upadhyay");
+
+
+// console.log(p.firstname);
+// console.log(p.lastname);
+
+
+
+
+
+//deafult values if not passing two values
+
+
+
+// class Person{
+//     constructor(firstname,lastname="doe"){
+//         this.firstname = firstname;
+//         this.lastname = lastname;
+//     }
+// }
+
+// let p= new Person("Riya");
+
+
+// console.log(p.firstname);
+// console.log(p.lastname);
+
+//Methodssss
+
+
+//funciton creation inside the class 
+//fun on a class are called methods we do not use funtion keyword 
+
+
+// class Person{
+//         constructor(firstname,lastname="doe"){
+//             this.firstname = firstname;
+//             this.lastname = lastname;
+//         }
+    
+
+
+//     greet(){
+//         console.log("Hii there");
+//     }
+
+// }
+
+//     let p= new Person("Riya","upadhyay");
+//     p.greet();
+
+
+
+//default arguments should be in last for position
+
+
+
+//methods
+//function keyword not used
+// class Person{
+//             constructor(firstname,lastname="doe"){
+//                 this.firstname = firstname;
+//                 this.lastname = lastname;
+//             }
+//   greet(){
+//     console.log("Hii there I'm",this.firstname);
+//   }
+// }
+
+//   let p= new Person("Riya","upadhyay");
+//       p.greet();
+
+
+
+
+// class Person{
+//                 constructor(firstname,lastname="doe"){
+//                     this.firstname = firstname;
+//                     this.lastname = lastname;
+//                 }
+//       greet(){
+//         console.log("Hii there I'm");
+//       }
+//     }
+
+// compliment(name,object){
+//     return "that is a woderful" + " " + name;
+// }
+
+
+// let p=new Person("hii","rya");
+
+// let compliment = p.compliment("harry","hat");
+// console.log(compliment);
+
+
+
+//PROPERTIES
+
+
+// class Person{
+//                 constructor(firstname,lastname="doe"){
+//                     this.firstname = firstname;
+//                     this.lastname = lastname;
+//                 }
+//             }
+
+
+//             //private
+// // class Person{
+// // #firstname;
+// // #lastname;
+// // }
+
+// // constructor(firstname,lastname){
+// //                     this.#firstname = firstname;
+// //                     this.#lastname = lastname;
+// //      }
+
+
+// // let p= new Person("Maria","saga");
+// // console.log(p.firstname); undefined
+
+
+// //private shown by methodus only
+
+
+// // constructor(firstname,lastname){
+// //     if(firstname.starstWith("M")){
+
+// //         this.#firstname = firstname;
+// //     }
+// //     else{
+
+// //         this.#firstname = "M" + firstname;
+// //     }
+// //     this.#lastname=lastname;
+// // }
+
+// //getters and setters
+
+
+// class Person{
+//     #firstname;
+//     #lastname;
+    
+    
+//     constructor(firstname,lastname){
+//         this.#firstname = firstname;
+//         this.#lastname = lastname;
+//          }
+
+         
+    // get firstname(){
+    //     return this.#firstname;
+    // }
+
+//     set firstname(firstname){
+//         return this.#firstname;
+//     }
+
+
+//     // get lastname(){
+//     //     return this.#lastname;
+//     // }
+
+//     set lastname(lastname){
+//         return this.#lastname;
+//     }
+// }
+
+
+// let p= new Person("Maria","saga");
+// console.log(p.firstname);//call get because no arguments passing if we pass then it will call set methods
+
+
+// console.log(p.firstname= "hello");//set method calling
+
+
+
+
+
+
